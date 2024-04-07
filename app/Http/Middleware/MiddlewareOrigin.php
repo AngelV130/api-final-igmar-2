845fa92d691831cb049eb19a39351ae4eebea97d
Angel Vargas <angelvrg130@gmail.com>
2024-04-07 15:06:27 -0600

    comm

--- app/Http/Middleware/MiddlewareOrigin.php
+++ app/Http/Middleware/MiddlewareOrigin.php
@@ -16,11 +16,11 @@ class MiddlewareOrigin
     public function handle(Request $request, Closure $next): Response
     {
         $allwedOrigin = explode(',',env('CORS_ALLOWED_ORIGINS'));
-        if(!in_array($request->headers->get('Origin'),$allwedOrigin) || !($request->headers->get('Origin') == null))
+        if(!in_array($request->headers->get('Origin'),$allwedOrigin) && !($request->headers->get('Origin') == null))
             return response([
                 'all'=> $allwedOrigin,
                 'origin'=> $request->headers->get('Origin'),
-                'ok' => !($request->headers->get('Origin') == null)
+                'ok' => !in_array($request->headers->get('Origin'),$allwedOrigin) && !($request->headers->get('Origin') == null)
             ], 404);
         return $next($request);
     }
